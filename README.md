25b8a47a4b6aaf89219b519076ba4e8463b72e9f
juruocjl <3024182971@qq.com>
2025-10-28 16:30:36 +0800

    ts

--- README.md
+++ README.md
@@ -2,14 +2,5 @@
 
 ## How to start
 
-1. `uv venv` .
-2. `source .venv/bin/activate` .
-3. `uv sync` .
-4. `nb run` .
-
-## Export Env
-1. `uv pip freeze > requirements.txt`
-
-## Documentation
-
-See [Docs](https://nonebot.dev/)
+1. `uv sync` .
+4. `uv run nb run` .
--- plugins/cs/__init__.py
+++ plugins/cs/__init__.py
@@ -37,7 +37,6 @@ from openai import OpenAI
 import psutil
 import json
 from fuzzywuzzy import process
-import ts3
 import asyncio
 from meme_generator import Image, get_meme
 
@@ -52,13 +51,13 @@ logging.basicConfig(
 SeasonId = config.cs_season_id
 lastSeasonId = config.cs_last_season_id
 
-with open("data.html", 'r', encoding='utf-8') as file:
+with open(Path("assets") / "data.html", 'r', encoding='utf-8') as file:
     data_content = file.read()
 
-with open("rank.html", 'r', encoding='utf-8') as file:
+with open(Path("assets") / "rank.html", 'r', encoding='utf-8') as file:
     rank_content = file.read().split("<!--SPLIT--->")
 
-with open("matches.html", 'r', encoding='utf-8') as file:
+with open(Path("assets") / "matches.html", 'r', encoding='utf-8') as file:
     matches_content = file.read().split("<!--SPLIT--->")
 
 
@@ -1466,8 +1465,6 @@ aimem = on_command("ai记忆", priority=10, block=True)
 
 getstatus = on_command("状态", priority=10, block=True)
 
-getts = on_command("getts", priority=10, block=True)
-
 getpic = on_command("getpic", priority=10, block=True)
 
 addpic = on_command("addpic", priority=10, block=True)
@@ -2018,202 +2015,6 @@ async def getstatus_function(message: MessageEvent):
     可用内存: {status['memory']['available_gb']}GB
     ----------------------------------------"""]))
 
-
-URI = "telnet://serveradmin:" + config.cs_ts_pswd + "@localhost:10011"
-
-SID = 1
-
-class ChannelTreeNode(object):
-    """
-    Represents a channel or the virtual server in the channel tree of a virtual
-    server. Note, that this is a recursive data structure.
-
-    Common
-    ------
-
-    self.childs = List with the child *Channels*.
-
-    self.root = The *Channel* object, that is the root of the whole channel
-                tree.
-
-    Channel
-    -------
-
-    Represents a real channel.
-
-    self.info =  Dictionary with all informations about the channel obtained by
-                 ts3conn.channelinfo
-
-    self.parent = The parent channel, represented by another *Channel* object.
-
-    self.clients = List with dictionaries, that contains informations about the
-                   clients in this channel.
-
-    Root Channel
-    ------------
-
-    Represents the virtual server itself.
-
-    self.info = Dictionary with all informations about the virtual server
-                obtained by ts3conn.serverinfo
-
-    self.parent = None
-
-    self.clients = None
-
-    Usage
-    -----
-
-    >>> tree = ChannelTreeNode.build_tree(ts3conn, sid=1)
-
-    Todo
-    ----
-
-    * It's not sure, that the tree is always correct sorted.
-    """
-
-    def __init__(self, info, parent, root, clients=None):
-        """
-        Inits a new channel node.
-
-        If root is None, root is set to *self*.
-        """
-        self.info = info
-        self.childs = list()
-
-        # Init a root channel
-        if root is None:
-            self.parent = None
-            self.clients = None
-            self.root = self
-
-        # Init a real channel
-        else:
-            self.parent = parent
-            self.root = root
-            self.clients = clients if clients is not None else list()
-        return None
-
-    @classmethod
-    def init_root(cls, info):
-        """
-        Creates a the root node of a channel tree.
-        """
-        return cls(info, None, None, None)
-
-    def is_root(self):
-        """
-        Returns true, if this node is the root of a channel tree (the virtual
-        server).
-        """
-        return self.parent is None
-
-    def is_channel(self):
-        """
-        Returns true, if this node represents a real channel.
-        """
-        return self.parent is not None
-
-    @classmethod
-    def build_tree(cls, ts3conn, sid):
-        """
-        Returns the channel tree from the virtual server identified with
-        *sid*, using the *TS3Connection* ts3conn.
-        """
-        ts3conn.exec_("use", sid=sid, virtual=True)
-
-        serverinfo = ts3conn.query("serverinfo").first()
-        channellist = ts3conn.query("channellist").all()
-        clientlist = ts3conn.query("clientlist").all()
-
-        # channel id -> clients
-        clientlist = {cid: [client for client in clientlist \
-                            if client["cid"] == cid]
-                      for cid in map(lambda e: e["cid"], channellist)}
-
-        root = cls.init_root(serverinfo)
-        for channel in channellist:
-            channelinfo = ts3conn.query("channelinfo", cid=channel["cid"]).first()
-
-            # This makes sure, that *cid* is in the dictionary.
-            channelinfo.update(channel)
-
-            channel = cls(
-                info=channelinfo, parent=root, root=root,
-                clients=clientlist[channel["cid"]])
-            root.insert(channel)
-        return root
-
-    def insert(self, channel):
-        """
-        Inserts the channel in the tree.
-        """
-        self.root._insert(channel)
-        return None
-
-    def _insert(self, channel):
-        """
-        Inserts the channel recursivly in the channel tree.
-        Returns true, if the tree has been inserted.
-        """
-        # We assumed on previous insertions, that a channel is a direct child
-        # of the root, if we could not find the parent. Correct this, if ctree
-        # is the parent from one of these orpheans.
-        if self.is_root():
-            i = 0
-            while i < len(self.childs):
-                child = self.childs[i]
-                if channel.info["cid"] == child.info["pid"]:
-                    channel.childs.append(child)
-                    self.childs.pop(i)
-                else:
-                    i += 1
-
-        # This is not the root and the channel is a direct child of this one.
-        elif channel.info["pid"] == self.info["cid"]:
-            self.childs.append(channel)
-            return True
-
-        # Try to insert the channel recursive.
-        for child in self.childs:
-            if child._insert(channel):
-                return True
-
-        # If we could not find a parent in the whole tree, assume, that the
-        # channel is a child of the root.
-        if self.is_root():
-            self.childs.append(channel)
-        return False
-
-    def print(self, indent=0):
-        """
-        Prints the channel and it's subchannels recursive. If restore_order is
-        true, the child channels will be sorted before printing them.
-        """
-        result = ""
-        if self.is_root():
-            result += " "*(indent*3) + "|- " + self.info["virtualserver_name"] + "\n"
-        else:
-            result += " "*(indent*3) + "|- " + self.info["channel_name"] + "\n"
-            for client in self.clients:
-                # Ignore query clients
-                if client["client_type"] == "1":
-                    continue
-                result += " "*(indent*3+3) + "-> " + client["client_nickname"] + "\n"
-
-        for child in self.childs:
-            result += child.print(indent=indent + 1)
-        return result
-
-@getts.handle()
-async def getts_function():
-    """
-    Prints the channel tree of the virtual server, including all clients.
-    """
-    with ts3.query.TS3ServerConnection(URI) as ts3conn:
-        ts3conn.exec_("use", sid=SID)
-        tree = ChannelTreeNode.build_tree(ts3conn, SID)
-        await getts.finish(tree.print())
 
 Pic1 = PicDir("pic")
 lastpic = None
