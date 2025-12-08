ce6603a2f475e8b6ecf38219a7eba0dcbfb94224
juruocjl <3024182971@qq.com>
2025-12-08 21:47:00 +0800

    qwq

--- plugins/major_hw/__init__.py
+++ plugins/major_hw/__init__.py
@@ -17,6 +17,7 @@ from unicodedata import normalize
 import json
 import asyncio
 from pathlib import Path
+from typing import List, Tuple
 
 from .gen_win_matrix import gen_win_matrix
 from .simulate import simulate
@@ -67,7 +68,9 @@ class DataManager:
 
 db = DataManager()
 
-teamfile = Path(".") / "assets" / f"{config.major_stage}.json"
+major_stage_name = f"{config.major_name}-{config.major_stage}"
+
+teamfile = Path(".") / "assets" / f"{major_stage_name}.json"
 major_teams = []
 alias2full = {}
 try:
@@ -90,7 +93,7 @@ def get_name(wuzzyname):
 file_path = "result.txt"
 
 
-logger.info(f"{config.major_stage}, {major_teams}")
+logger.info(f"{major_stage_name}, {major_teams}")
 results, total_simulations = parse_simulation_results(file_path)
 logger.info(f"已加载 {total_simulations} 个模拟结果")
 
@@ -104,9 +107,28 @@ hwupd = on_command("更新作业", priority=10, block=True, permission=SUPERUSER
 simupd = on_command("更新模拟", priority=10, block=True, permission=SUPERUSER)
 hwout = on_command("作业导出", priority=10, block=True, permission=SUPERUSER)
 
+def to_emoji(val):
+    if val == 1.0:
+        return "✅"
+    elif val == 0.0:
+        return "❌"
+    else:
+        return "❔"
+
 @hwhelp.handle()
 async def hwhelp_funtion():
-    await hwhelp.finish(f"""当前状态 {config.major_stage}
+    if config.major_stage == "playoffs":
+        await hwhelp.finish(f"""当前状态 {major_stage_name}
+/做作业 四强，四强，四强，四强，二强，二强，冠军
+添加作业，可用队伍 {major_teams}
+/查看作业 [@某人]
+查看自己或某人作业概率
+/作业排名
+查看当前作业排名
+/赛事作业结果
+查看赛事整体结果""")
+    else:
+        await hwhelp.finish(f"""当前状态 {major_stage_name}
 /做作业 team30,team30,team31/team32,team31/team32,team31/team32,team31/team32,team31/team32,team31/team32,team03,team03
 添加作业，可用队伍 {major_teams}
 /查看作业 [@某人]
@@ -114,20 +136,65 @@ async def hwhelp_funtion():
 /作业排名
 查看当前作业排名
 /赛事作业结果
-查看赛事整体结果
-""")
+查看赛事整体结果""")
 
-def calc_val(uid: str):
-    if res := db.get_uid_hw(uid, config.major_stage):
-        teams = json.loads(res[2])
-        combo = {
-            '3-0': teams[: 2],
-            '3-1/3-2': teams[2: 8],
-            '0-3': teams[8: ]
-        }
-        correct_counts, prob_ge5, expected_value = evaluate_combination(combo, results)
-        db.set_uid_val(uid, config.major_stage, prob_ge5, expected_value)
-        return prob_ge5, expected_value
+def calc_val(uid: str) -> Tuple[float, float] | None:
+    if config.major_stage == "playoffs":
+        result: List[Tuple[str, str, str, str]] = json.loads(localstorage.get(f"hltvresult{config.major_event_id}", default="[]"))
+        result.reverse()
+        if res := db.get_uid_hw(uid, major_stage_name + "-quad"):
+            teams = json.loads(res[2])
+            win_teams = [team1 for team1, _, _, _ in result[33:37]]
+            loss_teams = [team2 for _, team2, _, _ in result[33:37]]
+            if len(result) >= 37:
+                correct = sum(1 for team in teams if team in win_teams)
+            else:
+                correct = float('nan')
+            prob_ge2 = float("nan")
+            if sum(1 for team in teams if team in loss_teams) > 2:
+                prob_ge2 = 0.0
+            if sum(1 for team in teams if team in win_teams) >= 2:
+                prob_ge2 = 1.0
+            db.set_uid_val(uid, major_stage_name + "-quad", prob_ge2, correct)
+        if res := db.get_uid_hw(uid, major_stage_name + "-semi"):
+            teams = json.loads(res[2])
+            win_teams = [team1 for team1, _, _, _ in result[37:39]]
+            loss_teams = [team2 for _, team2, _, _ in result[33:39]]
+            if len(result) >= 39:
+                correct = sum(1 for team in teams if team in win_teams)
+            else:
+                correct = float('nan')
+            prob_ge1 = float("nan")
+            if sum(1 for team in teams if team in loss_teams) > 1:
+                prob_ge1 = 0.0
+            if sum(1 for team in teams if team in win_teams) >= 1:
+                prob_ge1 = 1.0
+            db.set_uid_val(uid, major_stage_name + "-semi", prob_ge1, correct)
+        if res := db.get_uid_hw(uid, major_stage_name + "-final"):
+            teams = json.loads(res[2])
+            win_teams = [team1 for team1, _, _, _ in result[39:40]]
+            loss_teams = [team2 for _, team2, _, _ in result[33:40]]
+            if len(result) >= 40:
+                correct = sum(1 for team in teams if team in win_teams)
+            else:
+                correct = float('nan')
+            prob_ge1 = float("nan")
+            if sum(1 for team in teams if team in loss_teams) > 0:
+                prob_ge1 = 0.0
+            if sum(1 for team in teams if team in win_teams) >= 1:
+                prob_ge1 = 1.0
+            db.set_uid_val(uid, major_stage_name + "-final", prob_ge1, correct)
+    else:
+        if res := db.get_uid_hw(uid, major_stage_name):
+            teams = json.loads(res[2])
+            combo = {
+                '3-0': teams[: 2],
+                '3-1/3-2': teams[2: 8],
+                '0-3': teams[8: ]
+            }
+            correct_counts, prob_ge5, expected_value = evaluate_combination(combo, results)
+            db.set_uid_val(uid, major_stage_name, prob_ge5, expected_value)
+            return prob_ge5, expected_value
 
 @hwadd.handle()
 async def hwadd_function(message: MessageEvent, arg: Message = CommandArg()):
@@ -136,13 +203,25 @@ async def hwadd_function(message: MessageEvent, arg: Message = CommandArg()):
     for team in normalize('NFKC', arg.extract_plain_text()).split(','):
         teams.append(get_name(team))
     await hwadd.send(f"模糊匹配得到队伍 {teams}，请仔细核对")
-    logger.info(teams)
-    if len(teams) != 10 or len(set(teams)) != 10:
-        await hwadd.finish("请输入十只不同队伍")
-    db.add_hw(uid, config.major_stage, json.dumps(teams))
-    await hwadd.send("成功添加预测，开始计算概率")
-    prob_ge5, expected_value = calc_val(uid)
-    await hwadd.finish(f">= 5 的概率 = {prob_ge5:.6f}，正确数期望 = {expected_value:.6f}")
+    if config.major_stage == "playoffs":
+        if len(teams) != 7:
+            await hwadd.finish("请输入七只只不同队伍")
+        quad = teams[:4]
+        semi = teams[4:6]
+        final = teams[6:]
+        if len(set(quad)) != 4 or len(set(semi)) != 2 or len(set(final)) != 1:
+            await hwadd.finish("请输入正确的淘汰赛队伍数量")
+        db.add_hw(uid, major_stage_name + "-quad", json.dumps(quad))
+        db.add_hw(uid, major_stage_name + "-semi", json.dumps(semi))
+        db.add_hw(uid, major_stage_name + "-final", json.dumps(final))
+        calc_val(uid)
+    else:
+        if len(teams) != 10 or len(set(teams)) != 10:
+            await hwadd.finish("请输入十只不同队伍")
+        db.add_hw(uid, major_stage_name, json.dumps(teams))
+        await hwadd.send("成功添加预测，开始计算概率")
+        prob_ge5, expected_value = calc_val(uid)
+        await hwadd.finish(f">= 5 的概率 = {prob_ge5:.6f}，正确数期望 = {expected_value:.6f}")
 
 @hwsee.handle()
 async def hwsee_function(message: MessageEvent):
@@ -150,14 +229,24 @@ async def hwsee_function(message: MessageEvent):
     for seg in message.get_message():
         if seg.type == "at" and seg.data['qq'] != 'all':
             uid = seg.data['qq']
-    if res := db.get_uid_hw(uid, config.major_stage):
-        prob_ge5, expected_value = res[3:]
-        teams = json.loads(res[2])
-        text = f"3-0 {teams[:2]}\n"
-        text += f"3-1/3-2 {teams[2:8]}\n"
-        text += f"0-3 {teams[8:]}\n"
-        text += f">= 5 的概率 = {prob_ge5:.6f}，正确数期望 = {expected_value:.6f}"
-        await hwsee.finish(text)
+    if config.major_stage == "playoffs":
+        if quad := db.get_uid_hw(uid, major_stage_name + "-quad"):
+            if semi := db.get_uid_hw(uid, major_stage_name + "-semi"):
+                if final := db.get_uid_hw(uid, major_stage_name + "-final"):
+                    text = f"{to_emoji(quad[3])}四强：{json.loads(quad[2])}\n"
+                    text += f"{to_emoji(semi[3])}决赛：{json.loads(semi[2])}\n"
+                    text += f"{to_emoji(final[3])}冠军：{json.loads(final[2])}\n"
+                    await hwsee.finish(text.strip())
+    else:
+        if res := db.get_uid_hw(uid, major_stage_name):
+            prob_ge5, expected_value = res[3:]
+            teams = json.loads(res[2])
+            text = f"3-0 {teams[:2]}\n"
+            text += f"3-1/3-2 {teams[2:8]}\n"
+            text += f"0-3 {teams[8:]}\n"
+            text += f">= 5 的概率 = {prob_ge5:.6f}，正确数期望 = {expected_value:.6f}"
+            await hwsee.finish(text)
+    await hwsee.finish("该用户未提交作业")
 
 async def getcard(bot: Bot, gid: str, uid: str):
     info = await bot.get_group_member_info(group_id=gid, user_id=uid, no_cache=False)
@@ -168,9 +257,9 @@ async def getcard(bot: Bot, gid: str, uid: str):
 @hwrank.handle()
 async def hwrank_function(bot: Bot, message: GroupMessageEvent):
     gid = message.get_session_id().split('_')[1]
-    res = db.get_all_hw(config.major_stage)
+    res = db.get_all_hw(major_stage_name)
     res = sorted(res, key=lambda x: x[3], reverse=True)
-    text = f"{config.major_stage} 作业排行"
+    text = f"{major_stage_name} 作业排行"
     for member in res:
         try:
             uid = member[0]
@@ -188,7 +277,7 @@ async def hwupd_function():
     results, total_simulations = parse_simulation_results(file_path)
     logger.info(f"已加载 {total_simulations} 个模拟结果")
 
-    res = db.get_all_hw(config.major_stage)
+    res = db.get_all_hw(major_stage)
     await hwupd.send("开始重新计算所有作业")
     for member in res:
         calc_val(member[0])
@@ -205,43 +294,57 @@ async def event_update(event_id):
         logger.info(f"{event_id} updated")
         bot = get_bot()
         
-        for groupid in config.cs_group_list:
-            await bot.send_msg(
-                message_type="group",
-                group_id=groupid,
-                message="开始重新模拟"
-            )
-        
-        await asyncio.to_thread(gen_win_matrix, str(teamfile), json.loads(localstorage.get(f"hltvresult{config.major_event_id}", default="[]")))
-        await asyncio.to_thread(simulate, teamfile)
+        if config.major_stage == "playoffs":
+            res = db.get_all_hw(major_stage_name + "-quad")
+            for member in res:
+                calc_val(member[0])
+            
+            for groupid in config.cs_group_list:
+                await bot.send_msg(
+                    message_type="group",
+                    group_id=groupid,
+                    message=f"成功计算 {len(res)} 份作业"
+                )
+        else:
+            for groupid in config.cs_group_list:
+                await bot.send_msg(
+                    message_type="group",
+                    group_id=groupid,
+                    message="开始重新模拟"
+                )
+            
+            await asyncio.to_thread(gen_win_matrix, str(teamfile), json.loads(localstorage.get(f"hltvresult{config.major_event_id}", default="[]")))
+            await asyncio.to_thread(simulate, teamfile)
 
-        for groupid in config.cs_group_list:
-            await bot.send_msg(
-                message_type="group",
-                group_id=groupid,
-                message="新结果模拟完成"
-            )
-        
-        global results
-        results, total_simulations = parse_simulation_results(file_path)
-        logger.info(f"已加载 {total_simulations} 个模拟结果")
+            for groupid in config.cs_group_list:
+                await bot.send_msg(
+                    message_type="group",
+                    group_id=groupid,
+                    message="新结果模拟完成"
+                )
+            
+            global results
+            results, total_simulations = parse_simulation_results(file_path)
+            logger.info(f"已加载 {total_simulations} 个模拟结果")
 
-        res = db.get_all_hw(config.major_stage)
-        for member in res:
-            calc_val(member[0])
-        
-        for groupid in config.cs_group_list:
-            await bot.send_msg(
-                message_type="group",
-                group_id=groupid,
-                message=f"成功计算 {len(res)} 份作业"
-            )
+            res = db.get_all_hw(major_stage_name)
+            for member in res:
+                calc_val(member[0])
+            
+            for groupid in config.cs_group_list:
+                await bot.send_msg(
+                    message_type="group",
+                    group_id=groupid,
+                    message=f"成功计算 {len(res)} 份作业"
+                )
+
+major_all_stages = [f"{config.major_name}-{x}" for x in ["stage1", "stage2", "stage3", "playoffs-quad", "playoffs-semi", "playoffs-final"]]
 
 @allrank.handle()
 async def allrank_function(bot: Bot, message: GroupMessageEvent):
     gid = message.get_session_id().split('_')[1]
     res = {}
-    for stage in config.major_all_stages:
+    for stage in major_all_stages:
         allres = db.get_all_hw(stage)
         for uid, _, _, wr, _ in allres:
             if uid not in res:
@@ -249,13 +352,8 @@ async def allrank_function(bot: Bot, message: GroupMessageEvent):
             res[uid][stage] = wr
     text = ""
     for uid, data in res.items():
-        for stage in config.major_all_stages:
-            if stage in data and data[stage] == 0.0:
-                text += "❌"
-            elif stage in data and data[stage] == 1.0:
-                text += "✅"
-            else:
-                text += "❔"
+        for stage in major_all_stages:
+            text += to_emoji(data.get(stage, float("nan")))
         text += " "
         text += await getcard(bot, gid, uid)
         text += "\n"
@@ -265,7 +363,7 @@ async def allrank_function(bot: Bot, message: GroupMessageEvent):
 async def hwout_function(bot: Bot, args: Message = CommandArg()):
     params = args.extract_plain_text().strip().split()
     gid = params[0]
-    stage = params[1] if len(params) > 1 else config.major_stage
+    stage = params[1] if len(params) > 1 else major_stage_name
     eventid = params[2] if len(params) > 2 else config.major_event_id
 
     res = db.get_all_hw(stage)
--- plugins/major_hw/config.py
+++ plugins/major_hw/config.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Config(BaseModel):
     """Plugin Config Here"""
+    major_name: str
     major_stage: str
-    major_all_stages: List[str]
     major_event_id: int
     cs_group_list: List[int]
