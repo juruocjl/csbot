2a241f96b71397394b6f29baae420af2c6e26ab9
juruocjl <3024182971@qq.com>
2026-01-05 12:36:45 +0800

    qwq

--- plugins/cs_db_upd/__init__.py
+++ plugins/cs_db_upd/__init__.py
@@ -631,7 +631,7 @@ qwqqwq = on_command("qwqqwq", permission=SUPERUSER, priority=5)
 
 @qwqqwq.handle()
 async def _(event):
-    stmt = select(SteamDetailInfo.steamid).distinct().where(SteamDetailInfo.pvpScore>2400)
+    stmt = select(SteamDetailInfo.steamid).distinct().where(SteamDetailInfo.pvpScore>2400).where(SteamDetailInfo.SeasonId==lastSeasonId)
     async with async_session_factory() as session:
         result = await session.execute(stmt)
         rows = result.scalars().all()
@@ -640,4 +640,19 @@ async def _(event):
             logger.info(f"is S: {row} ")
             async with async_session_factory() as session:
                 async with session.begin():
-                    await db._update_stats_card(row, session, interval=0)
+                    url = "https://api.wmpvp.com/api/csgo/home/pvp/detailStats/v2"
+                    header = {
+                        "appversion": "3.5.4.172",
+                        "token":config.cs_wmtoken
+                    }
+                    payload = {
+                        "mySteamId": config.cs_mysteam_id,
+                        "toSteamId": row,
+                        "csgoSeasonId": lastSeasonId,
+                    }
+                    async with get_session().post(url,headers=header,json=payload) as result:
+                        data = await result.json()
+                    if data["statusCode"] != 0:
+                        raise RuntimeError("上赛季爬取失败：" + data["errorMessage"])
+                    await db._insert_detail_info(data["data"], session)
+                    await asyncio.sleep(0.2)
