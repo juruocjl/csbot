cf28ef119df86e5445457d9073a995a9b81aaf05
juruocjl <3024182971@qq.com>
2026-03-24 12:37:48 +0800

    qwq

--- plugins/steam_guard/config.py
+++ plugins/steam_guard/config.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 
 class Config(BaseModel):
     cs_steam_guard_enable_group_list: list[int] = Field(default_factory=list)
-    cs_steam_guard_target_user: str = ""
+    cs_steam_guard_target_user: int | str = ""
     cs_steam_guard_ban_after: int = 3
     cs_steam_guard_ban_duration: int = 600
     cs_steam_guard_warn_cooldown_seconds: int = 300
